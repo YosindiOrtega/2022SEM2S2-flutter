e8a34cf5620990ee1adc2a8ef05c02f264bfecba
YosindiOrtega <68757951+YosindiOrtega@users.noreply.github.com>
2022-09-15 23:31:39 -0500

    Update home.pages.dart

--- lib/pages/home.pages.dart
+++ lib/pages/home.pages.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
+
 import 'package:calculadora/models/boton.model.dart';
 import 'package:flutter/material.dart';
-
+import 'package:math_expressions/math_expressions.dart';
 import '../widgets/matrizBotones.widgets.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,86 +13,90 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String Resultado='Test variable';
+  String Resultado='';
+  String r='';
+  List <String>  historico=[];
+  bool newOperador=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Calculadora"),),
       body: Column(children: [
-        Expanded(
-          flex:4,
-          child: Container(
+        Expanded( //Historico
+          flex:3,
+          child: SizedBox( //Los container fueron cambiados por SizedBox
             height: 100,
-            color: Colors.red,
+            //color: Color.fromARGB(255, 171, 179, 171),
             child: Row(
               children: [
-                Text("Operaciones"),
+                Text( historico.reversed.toString().replaceAll(',', '').replaceAll('(', '').replaceAll(')', ''),
+                 style: TextStyle(
+                  fontSize: 30, ),),               
               ],
             ),
           ),
         ),
-        Expanded(
-          flex:2,
+        Expanded( //Franja para editar la operación
+          flex:1,
           child: Container(
             height: 100,
-            color: Colors.green,
+            color: Color.fromARGB(255, 210, 219, 210),
             child: Row(
               children: [
                 Text(Resultado, style: TextStyle(
                   fontSize: 30, 
-                  color: Colors.white,
-                  ),
-                  
-                  ),
+                  color: Colors.black,
+                  ), ),
               ],
             ),
           ),
         ),
-        Expanded(
-          flex: 4,
-          child: Container(
-            height: 100,
-            color: Colors.grey,
+        Expanded( //Botones
+          flex: 5,
+          child: SizedBox(
+            height: 100,            
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                filaBotones([
+                filaBotones([ //porcentaje y borrados
                   BotonModel(titulo: "%", metodo: (){
                     setState(() {
-                      Resultado +="%";
+                      Resultado +='/100';
                     });                    
                   } ),
                   BotonModel(titulo: "CE", metodo: (){
                     setState(() {
-                      Resultado +="CE";
+                      Resultado ='';
+                      historico=[];
                     });                    
                   } ),
                   BotonModel(titulo: "C", metodo: (){
                     setState(() {
-                      Resultado +="C";
+                      Resultado ='' ;
                     });                    
                   } ),
-                  BotonModel(titulo: "<--", metodo: (){
-                    setState(() {
-                      Resultado +="<--";
+                  BotonModel(titulo: "←", metodo: (){
+                    setState(() {                      
+                      Resultado =Resultado.substring(0,Resultado.length-1);                      
                     });                    
                   } )
                   ]),
-
-                filaBotones([
+                filaBotones([ //potencias y demas 
                   BotonModel(titulo: "1/x", metodo: (){
                     setState(() {
-                      Resultado +="1/x";
+                      Resultado ="1/$Resultado";
                     });                    
                   } ),
                   BotonModel(titulo: "x^2", metodo: (){
                     setState(() {
-                      Resultado +="x^2";
+                      Resultado +="^2";
                     });                    
                   } ),
                   BotonModel(titulo: "√x", metodo: (){
                     setState(() {
-                      Resultado +="√x";
+                      String resuTemp=Resultado;
+                      Resultado = sqrt(num.parse(Resultado)).toString(); //historial
+                      historico.add('√'+resuTemp +'='+Resultado +'\n');                         
                     });                    
                   } ),
                   BotonModel(titulo: "/", metodo: (){
@@ -102,38 +108,62 @@ class _HomePageState extends State<HomePage> {
                 filaBotones([
                   BotonModel(titulo: "7", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }
                       Resultado +="7";
                     });                    
                   } ),
                   BotonModel(titulo: "8", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }
                       Resultado +="8";
                     });                    
                   } ),
                   BotonModel(titulo: "9", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }
                       Resultado +="9";
                     });                    
                   } ),
                   BotonModel(titulo: "x", metodo: (){
                     setState(() {
-                      Resultado +="x";
+                      Resultado +="*";
                     });                    
                   } )
                   ]),
                 filaBotones([
                   BotonModel(titulo: "4", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }
                       Resultado +="4";
                     });                    
                   } ),
                   BotonModel(titulo: "5", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }
                       Resultado +="5";
                     });                    
                   } ),
                   BotonModel(titulo: "6", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }
                       Resultado +="6";
                     });                    
                   } ),
@@ -146,16 +176,28 @@ class _HomePageState extends State<HomePage> {
                 filaBotones([
                   BotonModel(titulo: "1", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }
                       Resultado +="1";
                     });                    
                   } ),
                   BotonModel(titulo: "2", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }
                       Resultado +="2";
                     });                    
                   } ),
                   BotonModel(titulo: "3", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }
                       Resultado +="3";
                     });                    
                   } ),
@@ -168,23 +210,41 @@ class _HomePageState extends State<HomePage> {
                 filaBotones([
                   BotonModel(titulo: "±", metodo: (){
                     setState(() {
-                      Resultado +="±";
+                      Resultado +="*(-1)";
                     });                    
                   } ),
                   BotonModel(titulo: "0", metodo: (){
                     setState(() {
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      } 
                       Resultado +="0";
                     });                    
                   } ),
                   BotonModel(titulo: ",", metodo: (){
                     setState(() {
-                      Resultado +=",";
+                      Resultado+='.';
+                      if(newOperador==true){
+                        Resultado='';
+                        newOperador=false;
+                      }                       
                     });                    
                   } ),
                   BotonModel(titulo: "=", metodo: (){
-                    setState(() {
-                      Resultado ="=";
-                    });                    
+                    setState(() {                      
+                      r=Resultado;                 
+                      Parser p = Parser();
+                            ContextModel cm =  ContextModel();
+                            Expression exp = p.parse(r);
+                            r = exp
+                                .evaluate(EvaluationType.REAL, cm)  //Aqui se hace la magia :D
+                                .toString();  
+                      historico.add(Resultado+'='+r +'\n');   
+                      Resultado= r;
+                                                               
+                    });  
+                    newOperador=true;                  
                   } )
                   ]),             
               ],
